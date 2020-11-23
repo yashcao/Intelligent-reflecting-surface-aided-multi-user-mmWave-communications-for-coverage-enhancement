0cf2664b89eadca8b9a95f9fb2b30914d4bb6574
曹小帅 <yashcao@163.com>
2020-11-23 21:27:27 +0800

    Update FF_User.m

--- FF_User.m
+++ FF_User.m
@@ -9,17 +9,13 @@ kappa = a+10*b*log10(d)+normrnd(0, sigma, 1);
 
 % far-field array
 angU_e = rand(1, K)-0.5;                         % elevation sin_e
-%angU_a = (rand(1, K)-0.5).*cos(asin(angU_e));    % azimuth cos_a*cos_e
 angU_a = rand(1, K)-0.5;    % azimuth cos_a*cos_e
 
 % array steering vector
 arrU_a = (1/sqrt(Mx))*exp(-2*1i*pi*angU_a'*((0:Mx-1)-(Mx-1)/2))';
 arrU_e = (1/sqrt(My))*exp(-2*1i*pi*angU_e'*((0:My-1)-(My-1)/2))';
-% arrU_a = (1/sqrt(Mx))*exp(-2*1i*pi*angU_a'*(0:Mx-1))';
-% arrU_e = (1/sqrt(My))*exp(-2*1i*pi*angU_e'*(0:My-1))';
 
 % loss gain
-%beta_user = sqrt(1/2).*(randn(K, 1) +1i*randn(K, 1));
 beta_user = sqrt(M)*lambda_gain*(sqrt(1/2*10.^(-0.1*kappa))).*(randn(K, 1)+1j*randn(K, 1));
 
 for k=1:K
