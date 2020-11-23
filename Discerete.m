3b7650e878faa46f526c07a689a93f92a7d37408
曹小帅 <yashcao@163.com>
2020-11-23 21:26:03 +0800

    Update Discerete.m

--- Discerete.m
+++ Discerete.m
@@ -1,18 +1,10 @@
 function [ Theta_dis] = Discerete( bit, Theta_opt )
-%DISCERETE 此处显示有关此函数的摘要
+%DISCERETE 
 
 F_set = 2*pi*(0:(2^bit-1))/(2^bit);
 
 Theta_ang = angle(Theta_opt)+pi;
 
-% len = length(Theta_ang);
-% Theta_dis = zeros(len, 1);
-% 
-% for i=1:len
-%     Theta_d = abs(Theta_ang(i)-F_set);
-%     [~, I] = min(Theta_d);
-%     Theta_dis(i) = exp(1i*F_set(I));
-% end
 
 size_Theta = size(Theta_ang);
 Theta_dis = zeros(size_Theta);
